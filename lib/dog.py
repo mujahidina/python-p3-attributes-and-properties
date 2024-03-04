46dfe91b178c32a8491b5b132cd4736c0f2277ed
mujahidina <mujahidabdi5070@gmail.com>
2024-03-04 10:46:28 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,44 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+
+    def __init__(self, name="fido", breed="Beagle"):
+        self.name = self.set_name(name)
+        self.breed = self.set_breed(breed)
+
+      
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25 :
+            return name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+
+    def set_breed(self, breed):
+        if type(breed) == str and breed  in APPROVED_BREEDS:
+            return breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+fido = Dog(name ="fido", breed="Chihuahua")
+print(fido.name)
+print(fido.breed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,31 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+
+
+    def __init__(self, name="Guido", job="Sales"):
+        self.name = self.set_name(name)
+        self.job = self.set_job(job)
+
+      
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25 :
+            return name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+
+    def set_job(self, job):
+        if type(job) == str and job  in APPROVED_JOBS:
+            return job
+        else:
+            print("Job must be in list of approved jobs.")
+            
+
+mujahid = Person(name="mujahid abdi", job="teacher")
+print(mujahid.name)
+print(mujahid.job)
+
+
